dd47b0ee47a79be71d3ccb3aa1cfb908a36e0d1e
Antonio <achamorro@wecodeteam.com>
2019-09-12 17:52:19 +0200

    Add securization of controller

--- CleanArchDemo/CleanArch.Mvc/Controllers/CourseController.cs
+++ CleanArchDemo/CleanArch.Mvc/Controllers/CourseController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CleanArch.Mvc.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private ICourseService _courseService;
