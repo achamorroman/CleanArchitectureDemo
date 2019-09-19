936932e3f82f758c95b236bd4c5159536798ce53
Antonio <achamorro@wecodeteam.com>
2019-09-19 17:34:21 +0200

    In memory bus

--- CleanArchDemo/CleanArch.Infra.Bus/InMemoryBus.cs
+++ CleanArchDemo/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
+using MediatR;
 
 namespace CleanArch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
